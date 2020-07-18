6acd743a1d53b84ec5df15ba24c5d7d1e247f242
Bent André Solheim <bent.andre.solheim@kodehuset.no>
2020-07-18 21:11:56 +0200

    Connecting to web site via https

--- README.md
+++ README.md
@@ -6,5 +6,8 @@ Code for the Arduino MKR GSM 1400 for reading water temperature and dispatching 
 Create file src/secrets.h with contents
 
     #define SECRET_PINNUMBER "XXXX"
+    #define SECRET_GPRS_APN "telenor.smart"
+    #define SECRET_GPRS_LOGIN ""
+    #define SECRET_GPRS_PASSWORD ""
     
 to build.
--- src/main.cpp
+++ src/main.cpp
@@ -4,120 +4,80 @@
 
 // Please enter your sensitive data in the Secret tab or arduino_secrets.h
 // PIN Number
-const char PINNUMBER[] = SECRET_PINNUMBER;
+const char PINNUMBER[]     = SECRET_PINNUMBER;
+// APN data
+const char GPRS_APN[]      = SECRET_GPRS_APN;
+const char GPRS_LOGIN[]    = SECRET_GPRS_LOGIN;
+const char GPRS_PASSWORD[] = SECRET_GPRS_PASSWORD;
 
+// initialize the library instance
+GSMSSLClient client;
+GPRS gprs;
 GSM gsmAccess;
-GSM_SMS sms;
-GSMPIN gsmPin;
 
-/*
-  Read input serial
- */
-int readSerial(char result[]) {
-    int i = 0;
-    while (1) {
-        while (Serial.available() > 0) {
-            char inChar = Serial.read();
-            if (inChar == '\n') {
-                result[i] = '\0';
-                Serial.flush();
-                return 0;
-            }
-            if (inChar != '\r') {
-                result[i] = inChar;
-                i++;
-            }
-        }
-    }
-}
-
-void initLed() {
-    pinMode(LED_BUILTIN, OUTPUT);
-}
-
-void blinkLed(int ms) {
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(ms);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(ms);
-}
+// URL, path and port (for example: arduino.cc)
+char server[] = "httpbin.org";
+char path[] = "/anything";
+int port = 443; // port 443 is the default for HTTPS
 
 void setup() {
-    initLed();
-    blinkLed(500);
-    // initialize serial communications and wait for port to open
+    // initialize serial communications and wait for port to open:
     Serial.begin(9600);
-    while (!Serial) { ; // wait for serial port to connect. Needed for native USB port only
-    }
-    blinkLed(500);
-
-    Serial.println("SMS Messages Sender");
-    //Serial.println(PINNUMBER);
-
-    gsmPin.begin();
-    Serial.println("Restarted modem");
-    Serial.println(gsmPin.getPINUsed());
-    Serial.println(gsmPin.checkReg());
-    Serial.println(gsmPin.isPIN());
-    if (gsmPin.checkPIN(PINNUMBER) == 0) {
-        Serial.println("Pin OK");
-    } else {
-        Serial.println("Pin incorrect");
+    while (!Serial) {
+        ; // wait for serial port to connect. Needed for native USB port only
     }
 
+    Serial.println("Starting Arduino web client.");
     // connection state
     bool connected = false;
 
-    // Start GSM shield
-    // If your SIM has PIN, pass it as a parameter of begin() in quotes
+    // After starting the modem with GSM.begin()
+    // attach the shield to the GPRS network with the APN, login and password
     while (!connected) {
-        Serial.println("Connecting...");
-        int status = gsmAccess.begin(PINNUMBER);
-        if (status == GSM_READY) {
+        if ((gsmAccess.begin(PINNUMBER) == GSM_READY) &&
+            (gprs.attachGPRS(GPRS_APN, GPRS_LOGIN, GPRS_PASSWORD) == GPRS_READY)) {
             connected = true;
         } else {
-            if (status == ERROR) {
-                Serial.println("ERROR");
-            } else if (status == CONNECTING) {
-                Serial.println("CONNECTING");
-            } else if (status == GSM_READY) {
-                Serial.println("GSM_READY");
-            } else if (status == GPRS_READY) {
-                Serial.println("GPRS_READY");
-            } else if (status == TRANSPARENT_CONNECTED) {
-                Serial.println("TRANSPARENT_CONNECTED");
-            } else if (status == IDLE) {
-                Serial.println("IDLE");
-            } else {
-                Serial.println("Unknown status");
-            }
+            Serial.println("Not connected");
             delay(1000);
         }
     }
 
-    Serial.println("GSM initialized");
+    Serial.println("connecting...");
+
+    // if you get a connection, report back via serial:
+    if (client.connect(server, port)) {
+        Serial.println("connected");
+        // Make a HTTP request:
+        client.print("GET ");
+        client.print(path);
+        client.println(" HTTP/1.1");
+        client.print("Host: ");
+        client.println(server);
+        client.println("Connection: close");
+        client.println();
+    } else {
+        // if you didn't get a connection to the server:
+        Serial.println("connection failed");
+    }
 }
 
 void loop() {
+    // if there are incoming bytes available
+    // from the server, read them and print them:
+    if (client.available()) {
+        char c = client.read();
+        Serial.print(c);
+    }
 
-    Serial.print("Enter a mobile number: ");
-    char remoteNum[20];  // telephone number to send sms
-    readSerial(remoteNum);
-    Serial.println(remoteNum);
+    // if the server's disconnected, stop the client:
+    if (!client.available() && !client.connected()) {
+        Serial.println();
+        Serial.println("disconnecting.");
+        client.stop();
 
-    // sms text
-    Serial.print("Now, enter SMS content: ");
-    char txtMsg[200];
-    readSerial(txtMsg);
-    Serial.println("SENDING");
-    Serial.println();
-    Serial.println("Message:");
-    Serial.println(txtMsg);
-
-    // send the message
-    sms.beginSMS(remoteNum);
-    sms.print(txtMsg);
-    sms.endSMS();
-    Serial.println("\nCOMPLETE!\n");
+        // do nothing forevermore:
+        for (;;)
+            ;
+    }
 }
-
